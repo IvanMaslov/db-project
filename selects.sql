e578456046f6e19d179c73f6bcd6c923dc7039d3
Ivan <maslov.ivan.2000@niuitmo.ru>
2022-01-10 20:07:18 +0300

    More updates and some selects

--- selects.sql
+++ selects.sql
@@ -1,3 +1,16 @@
+-- преступники и статья
+select name, num
+from resolution r
+         natural join (select actId, num from act) a
+         join citizen c on r.blameCitizenId = c.citizenId
+where isGuilty = true;
+
+-- преступники, число предьявленных обвинений и число осуждений
+select name, count(isGuilty) as blamed, sum(isGuilty) as imprisoned
+from resolution r
+         join citizen c on r.blameCitizenId = c.citizenId
+group by citizenId;
+
 -- лучший адвокат
 select name, 1.0 - cast(sum(isGuilty) as float) / cast(count(*) as float) as successRate
 from resolution r
@@ -16,7 +29,7 @@ group by prosecutorId
 order by successRate desc
 limit 1;
 
--- самый случайный судья
+-- случайный судья
 -- Chaotic_lawful
 select name, abs(0.5 - cast(sum(isGuilty) as float) / cast(count(*) as float)) as randomRate
 from resolution r
@@ -35,7 +48,6 @@ from (select name, sum(isGuilty) as imprisonments
       group by citizenId) sub
 where imprisonments >= 3;
 
-
 -- пара судьи и прокурора, которые отправили в тюрьму наибольшее количество людей
 -- Best_friends
 select name as judgeName, prosecutorName, imprisonment
@@ -49,4 +61,3 @@ from (select name as prosecutorName, judgeId, imprisonment
             limit 1) pj
                join citizen c on pj.prosecutorId = c.citizenId) pj2
          join citizen c on pj2.judgeId = c.citizenId
-
--- updates.sql
+++ updates.sql
@@ -1,5 +1,6 @@
 delimiter //
 
+
 -- обновить паспорт
 create procedure updateCitizenPassport($citizenId INT, $newPassport CHAR(20))
     contains sql
@@ -8,6 +9,7 @@ begin
     set passport = $newPassport
     where citizenId = $citizenId;
 end//
+
 
 -- повышение квалификации
 create procedure upgradeJudgeClass($judgeId INT)
@@ -29,7 +31,40 @@ begin
     insert into prosecutor(prosecutorId, rnk, citizenId) VALUE (id, prosecutorRank, $citizenId);
 end //
 
+
+create procedure amnesty($citizenName VARCHAR(50))
+    contains sql
+begin
+    set transaction isolation level read uncommitted;
+    start transaction;
+    update resolution
+    set isGuilty = false
+    where (exists(select * from citizen where citizenId = blameCitizenId and name = $citizenName));
+    commit;
+end //
+
+
+-- забрать паспорта у всех осужденных по статье
+create procedure theGreatRepression($actNumber INT)
+    contains sql
+begin
+    set transaction isolation level read uncommitted;
+    start transaction;
+    update citizen
+    set passport = 'NO_PASSPORT'
+    where (exists(select *
+                  from resolution r
+                           natural join (select num, actId from act) a
+                  where r.blameCitizenId = citizenId
+                    and r.isGuilty is true
+                    and a.num = $actNumber));
+    commit;
+end //
+
+
 delimiter ;
 call updateCitizenPassport(3, 'PASSPORT_000003_NEW');
 call upgradeJudgeClass(3);
 call callToArms(101);
+call amnesty('B Bill Buffalo');
+call theGreatRepression(159);
